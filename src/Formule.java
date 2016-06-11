d5f37736b757f2af19216a0e2181fd639860b5b9
Anatole Dahan <adahan@me.com>
2016-06-11 16:36:49 +0200

    Initial commit

---
+++ src/Formule.java
@@ -0,0 +1,14 @@
+/**
+ * Created by anatole on 11/06/2016.
+ */
+public class Formule {
+    String[] variables;
+
+
+    public Formule(String url){
+        
+    }
+    public void parseDimacs(String dimacs){
+
+    }
+}
